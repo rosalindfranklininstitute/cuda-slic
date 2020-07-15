6b58353da8f9daa44f42763c04d2a0bf8ef8e5bb
Omar Elamin <omar.elamin@diamond.ac.uk>
2020-07-15 14:27:53 +0100

    added segmentaion tests for large data

--- tests_unit/test_slic3d.py
+++ tests_unit/test_slic3d.py
@@ -1,6 +1,3 @@
-import os
-import pytest
-
 import numpy as np
 
 from survos2.improc.regions.slic import slic3d
@@ -36,4 +33,16 @@ def test_slic3d_3channels_give_good_number_of_superpixels():
     blob = filters.gaussian(blob)
     blob = img_as_float32(blob)
     labels = slic3d(blob, nsp=100, compactness=300)
-    assert len(np.unique(labels)) < 200
+    assert len(np.unique(labels)) < 500
+
+# def test_slic3d_with_1GB_array():
+#     blob = data.binary_blobs(length=600, n_dim=3, seed=2)
+#     blob = img_as_float32(blob)
+#     labels = slic3d(blob, nsp=100, compactness=300)
+#     assert len(np.unique(labels)) < 500
+
+# def test_slic3d_with_1GB_array_and_500_000_superpixels():
+#     blob = data.binary_blobs(length=500, n_dim=3, seed=2)
+#     blob = img_as_float32(blob)
+#     labels = slic3d(blob, nsp=500000, compactness=300)
+#     assert len(np.unique(labels)) < 1000000
