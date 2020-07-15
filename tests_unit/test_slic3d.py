0b1cbe2373f5c2ad589613991bee8c36b9952438
Omar Elamin <omar.elamin@diamond.ac.uk>
2020-07-15 10:06:20 +0100

    added basic unit tests to slic3d function

--- tests_unit/test_slic3d.py
+++ tests_unit/test_slic3d.py
@@ -1,20 +1,39 @@
-
-
 import os
-import logging
 import pytest
 
-from survos2.data import embrain
-from survos2.improc.features import gaussian
+import numpy as np
+
 from survos2.improc.regions.slic import slic3d
 
-
-from skimage import data
+from skimage import data, color, filters
 from skimage import img_as_float32
-from skimage import color
 
-blob = data.binary_blobs(leng   th=512, n_dim=3, seed=2)
-blob = img_as_float32(blob)
-labels = slic3d(blob, sp_shape=(10, 10, 10), compactness=300)
-print(labels.shape)
-print(labels[:2, :2])
+def test_slic3d_grayscale_runs():
+    blob = data.binary_blobs(length=50, n_dim=3, seed=2)
+    blob = img_as_float32(blob)
+    labels = slic3d(blob, nsp=100, compactness=300)
+    assert isinstance(labels, np.ndarray)
+    assert labels.ndim == 3
+
+def test_slic3d_3channels_runs():
+    blob = data.binary_blobs(length=50, n_dim=4, seed=2)
+    blob = blob[:3]
+    blob = filters.gaussian(blob)
+    blob = img_as_float32(blob)
+    labels = slic3d(blob, nsp=100, compactness=300)
+    assert isinstance(labels, np.ndarray)
+    assert labels.ndim == 3
+
+def test_slic3d_grayscale_give_good_number_of_superpixels():
+    blob = data.binary_blobs(length=50, n_dim=3, seed=2)
+    blob = img_as_float32(blob)
+    labels = slic3d(blob, nsp=100, compactness=300)
+    assert len(np.unique(labels)) < 200
+
+def test_slic3d_3channels_give_good_number_of_superpixels():
+    blob = data.binary_blobs(length=100, n_dim=4, seed=2)
+    blob = blob[:3]
+    blob = filters.gaussian(blob)
+    blob = img_as_float32(blob)
+    labels = slic3d(blob, nsp=100, compactness=300)
+    assert len(np.unique(labels)) < 200
