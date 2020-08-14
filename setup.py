226d1f0d9306559ac6227189a7c4854d226f5ab3
ovs72384 <omar.elamin@diamond.ac.uk>
2020-08-14 20:29:36 +0100

    adding setup.py preparing to distribute liberary on pypi

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cuda_slic",
+    version="0.0.1a1",
+    python_requires=">=3.5",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+
+    include_package_data = True,
+
+    install_requires=[
+        "numpy",
+        "scikit-image",
+        "pycuda>=2019.1.2",
+    ],
+
+    # metadata to display on PyPI
+
+    author="Omar Elamin",
+    author_email="omar.elamin@diamond.ac.uk",
+    description="CUDA implementation of the SLIC segmentaion algorithm.",
+    keywords="segmentation fast cuda slic clustering kmeans",
+    url="https://gitlab.stfc.ac.uk/RosalindFranklinInstitute/cuda-slic",   # project home page, if any
+    classifiers=[
+        "License :: OSI Approved :: Apache 2.0",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+
+    options={"bdist_wheel": {"universal": "1"}},
+
+    # could also include long_description, download_url, etc.
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
