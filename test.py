bf809edbe9b17a50dbb0e599cba9977bf9644210
ovs72384 <omar.elamin@diamond.ac.uk>
2020-08-28 15:14:57 +0100

    CUPY does not support passing vector types by value Tried passing pointer then dereference but its even slower. The method tested is shown in test.py

--- test.py
+++ test.py
@@ -3,20 +3,23 @@ import cupy as cp
 
 kernel = """
 extern "C" {
-    constexpr __device__ int3 ii{1,2,3};
 
     __global__
-    void init_vec() {
-    const int3 i = ii;
-    printf("i.x == %d", i.x);
+    void init_vec(int3* ii)
+    {
+        int3 i = *ii;
+        printf("i.x == %d", i.x);
+        printf("i.y == %d", i.y);
+        printf("i.z == %d", i.z);
     }
 
 }
 """
 mod = cp.RawModule(code=kernel, options=("-std=c++14",))
-vv = mod.get_function('vv')
+vv = mod.get_function('init_vec')
+ii = cp.asarray([1,2,3], dtype=cp.int32)
 vv(
     (1,),
     (1,),
-    (),
+    (ii),
 )
